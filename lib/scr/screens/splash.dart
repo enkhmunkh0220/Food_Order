af6df858263dd2a82ab0da5045a4c001ec363566
Hunraa Bumsuren <hunraa@Hunraas-Air.lan>
2020-12-28 14:44:44 +0100

    first commit

---
+++ lib/scr/screens/splash.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:Food_Order/scr/helpers/style.dart';
+import 'package:Food_Order/scr/widgets/custom_text.dart';
+import 'package:Food_Order/scr/widgets/loading.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CustomText(text: "Loading"),
+            Loading(),
+          ],
+        ));
+  }
+}
